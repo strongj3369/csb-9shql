d3e880b222cb970d014eb1f630bce248b64ea817
Code Sandbox <hello@codesandbox.io>
2022-01-08 03:11:47 +0000

    Deployment from Codesandbox.

---
+++ precache-manifest.1c323f822d491eee00f33dfc32740c3e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "67f8fc81a74617c4801c",
+    "url": "https://strongj3369.github.io/csb-9shql/static/css/main.9fed1463.chunk.css"
+  },
+  {
+    "revision": "67f8fc81a74617c4801c",
+    "url": "https://strongj3369.github.io/csb-9shql/static/js/main.7bcedb3f.chunk.js"
+  },
+  {
+    "revision": "5a2dcc1f80a427acdb49",
+    "url": "https://strongj3369.github.io/csb-9shql/static/js/runtime~main.7a5a17db.js"
+  },
+  {
+    "revision": "17cf15d4dc312fd33203",
+    "url": "https://strongj3369.github.io/csb-9shql/static/js/2.e7ef6a41.chunk.js"
+  },
+  {
+    "revision": "bb3d8be5719afc07e7f386da253312be",
+    "url": "https://strongj3369.github.io/csb-9shql/index.html"
+  }
+];
